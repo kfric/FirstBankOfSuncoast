f9e482d6312f71617aa0277a693569911f2b9d72
Karl Frick <karl.frick5@gmail.com>
2021-04-22 16:47:34 -0400

    Set up Prompt methods

--- Program.cs
+++ Program.cs
@@ -2,52 +2,120 @@
 
 namespace FirstBankOfSuncoast
 {
+    class Transaction
+    {
+        // AcctType {get; set;}
+        public string AccountType { get; set; }
+
+        // Balance {get; set;}
+        public string Balance { get; set; }
+
+        // saving {get; set;}
+        public string SavingAccount { get; set; }
+        // checking acct
+        public string CheckingAccount { get; set; }
+    }
     class Program
     {
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("--- Welcome to First Bank of Suncoast ---");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("");
+        }
+        // method to prompt for string response
+        static string PromptForString(string prompt)
+        {
+            // write prompt
+            Console.WriteLine(prompt);
+            // read line and set it as the userInput
+            var userInput = Console.ReadLine();
+            // - return response
+            return userInput;
+        }
+
+        static int PromptForInterger(string prompt)
+        {
+            // method to prompt for int response
+            Console.WriteLine(prompt);
+            int userInput;
+            // - read line
+            var goodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+            if (goodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Invalid numbers default to 0");
+                return 0;
+            }
+
+            // - return response
+        }
+
+
+        // load past transactions from a file when it first starts.....save for last
         static void Main(string[] args)
         {
-            //       -- Promblem ---
-            // - The application should store a history of transactions in a SINGLE List<Transaction>. 
-            // Your task is to design the Transaction class to support both checking and savings as well as deposits and withdraws.
-            // - The application should load past transactions from a file when it first starts.
-            // - As a user I should be able to see the list of transactions designated savings.
-            // - As a user I should be able to see the list of transactions designated checking.
-            // - Never allow withdrawing more money than is available. That is, we cannot allow our checking or 
-            // savings balances to go negative.
-            // - When prompting for an amount to deposit or withdraw always ensure the amount is positive. The value we 
-            // store in the Transaction shall be positive as well. (e.g. a Transaction that is a withdraw of 25 both inputs and 
-            // records a positive 25)
-            // - As a user I should have a menu option to make a deposit transaction for savings.
-            // - As a user I should have a menu option to make a deposit transaction for checking.
-            // - As a user I should have a menu option to make a withdraw transaction for savings.
-            // - As a user I should have a menu option to make a withdraw transaction for checking.
-            // - As a user I should have a menu option to see the balance of my savings and checking.
-            // - The application should, after each transaction, write all the transactions to a file. This is the same file the 
-            // application loads.
 
-            // -- Example -- 
-            // user is displayed $10 in checking and $50 in savings. user will have the option to either select [C]hecking
-            // or [S]avings. user will then have to select either [W]ithdraw or [D]epsoit. user wants to deposit $5 in checking
-            //   +$5 is added to the current balance of the checking acct. user wants selects check balance
-            //   and the balance of both checking and savings are dispalyed.
-            // user is displayed $10 in checking and $50 in savings. user will have the option to either select [C]hecking
-            // or [S]avings. user will then have to select either [W]ithdraw or [D]epsoit. user wants to withdraw $10 from savings 
-            //   acct. -$5 subtracted from the saving balance ($50). user wants to display the current balance
-            //   of both accts. checking balance of $10 and saving balance of $40 are displayed.
-            // user is displayed $10 in checking and $50 in savings. user will have the option to either select [C]hecking
-            // or [S]avings. user will then have to select either [W]ithdraw or [D]epsoit. user wants to withdraw $15 from their checking
-            //   acct. user withdraw amount ($15) is greater than the balance of checking ($10). user is notifed 
-            //   "insuffient funds" and is displayed the balance of their checking acct.
+            // display welcome banner
+            DisplayWelcome();
 
 
-            // -- Data --
-            // nouns:
-            // checkings
-            // - holds checking balance - int
-            // - shows all trans for this acct - string
-            // savings
-            // - holds savings balance - int
-            // - shows all trans for this acct - string
+
+
+
+
+
+
+            // prompt "Which acct would you like to update?: [C]hecking or [S]avings"
+            // prompt "Specified amount $:?"
+
+            // create new balance list to hold the values of the balance for checking and saving acct
+            // -add the collection of values to the list 
+
+            // create a menu
+            // "Please select from the followring: [D]eposit. [W]ithdraw. [V]eiw. [Q]uit.
+
+            // if input = V
+            // console.writeline("{name} has a balance of {balance}")
+
+            // else if = D
+            //   var acctDeposit = prompt for string "Which acct do you want to update?"
+            //   var acctToUpdate = acct by name in accts list with the name equal to the name to be updated
+            //   if null
+            //   return no acct by that name
+
+            //   else transAmount = prompt for int $"how much do you want to deposit in {acctToUpdate}?"
+
+            //   acctToUpdate.balance = transAmount + balance??
+
+            // else if = W
+            //   var acctDeposit = prompt for string "Which acct do you want to update?"
+            //   var acctToUpdate = acct by name in accts list with the name equal to the name to be update
+            //   if null
+            //   return no acct by that name
+
+            //   else transAmount = prompt for int $"how much do you want to deposit in {acctToUpdate}?"
+
+            //   acctToUpdate.balance = transAmount - balance??
+
+            //   else transAmount = prompt for int $"how much do you want to deposit in {acctToUpdate}?"
+
+            //   acctToUpdate.balance = transAmount + balance??
+
+
+
+            // else input = Q
+            // bool = false
+
+            // The application should, after each transaction, write all the transactions to a file.
+            // This is the same file the application loads.......save for last
+
+
+
 
         }
     }
