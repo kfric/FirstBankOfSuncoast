eed69e3968648fb422505a9cc652e58596da6016
Karl Frick <karl.frick5@gmail.com>
2021-04-25 16:44:09 -0400

    Cleaned up with switch loops

--- Program.cs
+++ Program.cs
@@ -95,145 +95,107 @@ namespace FirstBankOfSuncoast
 
                 var answer = Console.ReadLine().ToUpper();
 
-                // if input = Q
-                if (answer == "Q")
-                // bool = false
+                switch (answer)
                 {
-                    keepGoing = false;
-                }
-                // if input = V
-                else if (answer == "V")
-                {
-                    Console.WriteLine($"Checking acct total: ${CalculateBalance(transactions, "checking")}");
-                    Console.WriteLine($"Savings acct total: ${CalculateBalance(transactions, "savings")}");
-                }
-                else if (answer == "D")
-                {
-                    var tran = new Transaction();
 
-                    var acctToUpdate = PromptForString("[C]hecking or [S]avings?");
 
-                    // Transaction foundAcctToUpdate = tranactions.FirstOrDefault(tran => tran.AccountType == acctToUpdate);
+                    // if input = Q
+                    case "Q":
+                        // bool = false
 
-                    if (acctToUpdate == null)
-                    {
-                        Console.WriteLine("Please select either [C]hecking or [S]avings");
-                    }
-                    else
-                    {
-                        if (acctToUpdate == "C")
+                        keepGoing = false;
+                        break;
+                    // if input = V
+                    case "V":
+
+                        Console.WriteLine($"Checking acct total: ${CalculateBalance(transactions, "checking")}");
+                        Console.WriteLine($"Savings acct total: ${CalculateBalance(transactions, "savings")}");
+                        break;
+
+                    case "D":
+
+                        var tran = new Transaction();
+
+                        var acctToDeposit = PromptForString("[C]hecking or [S]avings?");
+
+                        // Transaction foundAcctToUpdate = tranactions.FirstOrDefault(tran => tran.AccountType == acctToUpdate);
+
+                        if (acctToDeposit == null)
                         {
-                            tran.AccountType = "checking";
-                            tran.TransactionType = "deposit";
-                            tran.TransactionAmount = PromptForInterger("How much do you want to deposit?");
-                            transactions.Add(tran);
-
-
+                            Console.WriteLine("Please select either [C]hecking or [S]avings");
                         }
-                        else if (acctToUpdate == "S")
+                        else
                         {
-                            tran.AccountType = "savings";
-                            tran.TransactionType = "deposit";
-                            tran.TransactionAmount = PromptForInterger("How much do you want to deposit?");
-                            transactions.Add(tran);
-                        }
-                    }
-
-                }
-                else if (answer == "W")
-                {
-                    var tran = new Transaction();
-
-                    var acctToUpdate = PromptForString("[C]hecking or [S]avings?");
-
-                    if (acctToUpdate == null)
-                    {
-                        Console.WriteLine("Please select either [C]hecking or [S]avings");
-                    }
-                    else
-                    {
-                        if (acctToUpdate == "C")
-                        {
-                            tran.AccountType = "checking";
-                            tran.TransactionType = "withdraw";
-                            tran.TransactionAmount = PromptForInterger("How much do you want to withdraw?");
-
-                            // create if statement so that the user cannot withdraw more than what is in the acct
-                            if (CalculateBalance(transactions, "checking") < tran.TransactionAmount)
+                            if (acctToDeposit == "C")
                             {
-                                Console.WriteLine("Insufficient funds");
-                            }
-                            else
-                            {
-                                transactions.Add(tran);
-                            }
-
-                        }
-                        else if (acctToUpdate == "S")
-                        {
-                            tran.AccountType = "savings";
-                            tran.TransactionType = "withdraw";
-                            tran.TransactionAmount = PromptForInterger("How much do you want to withdraw?");
-
-                            if (CalculateBalance(transactions, "savings") < tran.TransactionAmount)
-                            {
-                                Console.WriteLine("Insufficient Funds");
-                            }
-                            else
-                            {
+                                tran.AccountType = "checking";
+                                tran.TransactionType = "deposit";
+                                tran.TransactionAmount = PromptForInterger("How much do you want to deposit?");
                                 transactions.Add(tran);
 
+
+                            }
+                            else if (acctToDeposit == "S")
+                            {
+                                tran.AccountType = "savings";
+                                tran.TransactionType = "deposit";
+                                tran.TransactionAmount = PromptForInterger("How much do you want to deposit?");
+                                transactions.Add(tran);
                             }
                         }
-                    }
+
+                        break;
+                    case "W":
+
+                        var withdrawTran = new Transaction();
+
+                        var acctToWithdraw = PromptForString("[C]hecking or [S]avings?");
+
+                        if (acctToWithdraw == null)
+                        {
+                            Console.WriteLine("Please select either [C]hecking or [S]avings");
+                        }
+                        else
+                        {
+                            if (acctToWithdraw == "C")
+                            {
+                                withdrawTran.AccountType = "checking";
+                                withdrawTran.TransactionType = "withdraw";
+                                withdrawTran.TransactionAmount = PromptForInterger("How much do you want to withdraw?");
+
+                                // create if statement so that the user cannot withdraw more than what is in the acct
+                                if (CalculateBalance(transactions, "checking") < withdrawTran.TransactionAmount)
+                                {
+                                    Console.WriteLine($"Insufficient funds. Available: ${CalculateBalance(transactions, "checking")}");
+                                }
+                                else
+                                {
+                                    transactions.Add(withdrawTran);
+                                }
+
+                            }
+                            else if (acctToWithdraw == "S")
+                            {
+                                withdrawTran.AccountType = "savings";
+                                withdrawTran.TransactionType = "withdraw";
+                                withdrawTran.TransactionAmount = PromptForInterger("How much do you want to withdraw?");
+
+                                if (CalculateBalance(transactions, "savings") < withdrawTran.TransactionAmount)
+                                {
+                                    Console.WriteLine($"Insufficient Funds. Available: ${CalculateBalance(transactions, "savings")}");
+                                }
+                                else
+                                {
+                                    transactions.Add(withdrawTran);
+                                }
+                            }
+                        }
+                        break;
 
                 }
 
             }
-
-
-
-
-            // else if = D
-            //   var acctDeposit = prompt for string "Which acct do you want to update?"
-            //   var acctToUpdate = acct by name in accts list with the name equal to the name to be updated
-            //   if null
-            //   return no acct by that name
-
-            //   else transAmount = prompt for int $"how much do you want to deposit in {acctToUpdate}?"
-
-            //   acctToUpdate.balance = transAmount + balance??
-
-            // else if = W
-            //   var acctDeposit = prompt for string "Which acct do you want to update?"
-            //   var acctToUpdate = acct by name in accts list with the name equal to the name to be update
-            //   if null
-            //   return no acct by that name
-
-            //   else transAmount = prompt for int $"how much do you want to deposit in {acctToUpdate}?"
-
-            //   acctToUpdate.balance = transAmount - balance??
-
-            //   else transAmount = prompt for int $"how much do you want to deposit in {acctToUpdate}?"
-
-            //   acctToUpdate.balance = transAmount + balance??
-
-
-
-            // The application should, after each transaction, write all the transactions to a file.
-            // This is the same file the application loads.......save for last
-
-
-
-
-
         }
-
-
-
-
-
-
 
     }
 }
